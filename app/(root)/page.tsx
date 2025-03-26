b87ef33ec91c7f1e777b0a5c8633286eca0fb13f
Joerakesh <rakeshjoe52@gmail.com>
2025-03-26 21:54:02 +0530

    new file:   app/(root)/interview/[id]/feedback/page.tsx 	new file:   app/(root)/interview/[id]/page.tsx 	modified:   app/(root)/page.tsx 	modified:   components/Agent.tsx 	modified:   components/InterviewCard.tsx 	modified:   lib/actions/auth.action.ts 	new file:   lib/actions/general.action.ts 	modified:   types/index.d.ts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,14 @@
 import InterviewCard from "@/components/InterviewCard";
 import { Button } from "@/components/ui/button";
 import { dummyInterviews } from "@/constants";
-import {
-  getCurrentUser,
-  getInterviewByUserId,
-  getLatestInterviews,
-} from "@/lib/actions/auth.action";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-
+import {
+  getInterviewByUserId,
+  getLatestInterviews,
+} from "@/lib/actions/general.action";
 const page = async () => {
   const user = await getCurrentUser();
   const [userInterview, latestInterviews] = await Promise.all([
